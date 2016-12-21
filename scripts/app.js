38060a850f6e8f7cb61e7e69b24b9260e3df4994
Rinda Ko <rinda@Rindas-MacBook-Air.local>
2016-12-20 19:20:46 -0500

    uploading html and css files for personal website

---
+++ scripts/app.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  var $menu = $("#sidebar-wrapper");
+
+  $(document)
+    .on("click", ".js-menu-open", function() {
+      $menu.addClass("open");
+
+      return false;
+    })
+    .on("click", ".js-menu-close", function() {
+      $menu.removeClass("open");
+
+      return false;
+    });
+});
